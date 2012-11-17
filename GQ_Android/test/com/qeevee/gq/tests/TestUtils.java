8bfa33ad177cc3ea71184a2a76b00d3a6780d103
Holger Muegge <muegge@acm.org>
2012-11-17 22:33:26 +0100

    call method via refelection and get res strings

--- GQ_Android/test/com/qeevee/gq/tests/TestUtils.java
+++ GQ_Android/test/com/qeevee/gq/tests/TestUtils.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 
 import org.dom4j.Document;
@@ -22,114 +24,165 @@ import edu.bonn.mobilegaming.geoquest.mission.Mission;
 
 public class TestUtils {
 
-	public static Document loadTestGame(String gameName) {
-		Document document = null;
-		SAXReader reader = new SAXReader();
-		try {
-			File gameFile = getGameFile(gameName);
-			document = reader.read(gameFile);
-			GeoQuestApp.setRunningGameDir(new File(gameFile.getParent()));
-		} catch (DocumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return document;
+    public static Document loadTestGame(String gameName) {
+	Document document = null;
+	SAXReader reader = new SAXReader();
+	try {
+	    File gameFile = getGameFile(gameName);
+	    document = reader.read(gameFile);
+	    GeoQuestApp.setRunningGameDir(new File(gameFile.getParent()));
+	} catch (DocumentException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+	return document;
+    }
 
-	public static File getGameFile(String gameName) {
-		URL xmlFileURL = TestUtils.class.getResource("/testgames/" + gameName
-				+ "/game.xml");
-		if (xmlFileURL == null)
-			fail("Resource file not found for game: " + gameName);
-		return new File(xmlFileURL.getFile());
+    public static File getGameFile(String gameName) {
+	URL xmlFileURL = TestUtils.class.getResource("/testgames/" + gameName
+		+ "/game.xml");
+	if (xmlFileURL == null)
+	    fail("Resource file not found for game: " + gameName);
+	return new File(xmlFileURL.getFile());
+    }
+
+    /**
+     * Sets up the test for a single mission type using a prepared game file
+     * (game.xml) which must be stored in "testgames" directory and named
+     * "<missionName>Test".
+     * 
+     * @param missionType
+     *            must be a valid mission type for which a class exists in the
+     *            mission implementation package.
+     * @param missionID
+     * @return a new Activity object of the according type for the given mission
+     *         type name. You can for example directly call onCreate() upon it
+     *         to emulate the android framework behavior.
+     * @throws ClassNotFoundException
+     */
+    public static GeoQuestActivity setUpMissionTest(String missionType,
+						    String missionID) {
+	return setUpMissionTypeTest(missionType + "Test",
+				    missionType,
+				    missionID);
+    }
+
+    /**
+     * Sets up the test for a single mission type using a prepared game file
+     * (game.xml) which must be stored in "testgames" directory.
+     * 
+     * @param gameFileName
+     *            the filename of the xml game specification used for this test.
+     * @param missionType
+     *            must be a valid mission type for which a class exists in the
+     *            mission implementation package.
+     * @param missionID
+     * @return a new Activity object of the according type for the given mission
+     *         type name. You can for example directly call onCreate() upon it
+     *         to emulate the android framework behavior.
+     * @throws ClassNotFoundException
+     */
+    public static GeoQuestActivity setUpMissionTypeTest(String gameFileName,
+							String missionType,
+							String missionID) {
+	Class<?> missionClass = null;
+	GeoQuestActivity missionActivity = null;
+
+	try {
+	    missionClass = Class.forName(Mission.getPackageBaseName()
+		    + missionType);
+	    missionActivity = (GeoQuestActivity) missionClass.newInstance();
+	} catch (InstantiationException e) {
+	    throw new RuntimeException(e);
+	} catch (IllegalAccessException e) {
+	    throw new RuntimeException(e);
+	} catch (ClassNotFoundException e) {
+	    throw new RuntimeException(e);
 	}
-
-	/**
-	 * Sets up the test for a single mission type using a prepared game file
-	 * (game.xml) which must be stored in "testgames" directory and named
-	 * "<missionName>Test".
-	 * 
-	 * @param missionType
-	 *            must be a valid mission type for which a class exists in the
-	 *            mission implementation package.
-	 * @param missionID
-	 * @return a new Object of the according type for the given mission type
-	 *         name.
-	 * @throws ClassNotFoundException
-	 */
-	public static GeoQuestActivity setUpMissionTypeTest(String missionType,
-			String missionID) throws ClassNotFoundException {
-		return setUpMissionTypeTest(missionType + "Test", missionType,
+	Start start = new Start();
+	GeoQuestApp app = (GeoQuestApp) start.getApplication();
+	app.onCreate();
+	Mission.setMainActivity(start);
+	GameLoader.startGame(null,
+			     TestUtils.getGameFile(gameFileName));
+	Intent startMCQIntent = new Intent(start, missionClass);
+	startMCQIntent.putExtra("missionID",
 				missionID);
-	}
+	Robolectric.shadowOf(missionActivity).setIntent(startMCQIntent);
+	return missionActivity;
+    }
 
-	/**
-	 * Sets up the test for a single mission type using a prepared game file
-	 * (game.xml) which must be stored in "testgames" directory.
-	 * 
-	 * @param gameFileName
-	 *            the filename of the xml game specification used for this test.
-	 * @param missionType
-	 *            must be a valid mission type for which a class exists in the
-	 *            mission implementation package.
-	 * @param missionID
-	 * @return a new Object of the according type for the given mission type
-	 *         name.
-	 * @throws ClassNotFoundException
-	 */
-	public static GeoQuestActivity setUpMissionTypeTest(String gameFileName,
-			String missionType, String missionID) throws ClassNotFoundException {
-		Class<?> missionClass = Class.forName(Mission.getPackageBaseName()
-				+ missionType);
-		GeoQuestActivity missionActivity = null;
-		try {
-			missionActivity = (GeoQuestActivity) missionClass.newInstance();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Start start = new Start();
-		GeoQuestApp app = (GeoQuestApp) start.getApplication();
-		app.onCreate();
-		Mission.setMainActivity(start);
-		GameLoader.startGame(null, TestUtils.getGameFile(gameFileName));
-		Intent startMCQIntent = new Intent(start, missionClass);
-		startMCQIntent.putExtra("missionID", missionID);
-		Robolectric.shadowOf(missionActivity).setIntent(startMCQIntent);
-		return missionActivity;
+    /**
+     * Lets you access the values of private or protected fields in your tests.
+     * You will have to cast the resulting object down to the real type.
+     * 
+     * @param obj
+     * @param fieldName
+     * @return
+     */
+    public static Object getFieldValue(Object obj,
+				       String fieldName) {
+	Object value = null;
+	try {
+	    Field f = obj.getClass().getDeclaredField(fieldName);
+	    f.setAccessible(true);
+	    value = f.get(obj);
+	} catch (IllegalArgumentException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (SecurityException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (IllegalAccessException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (NoSuchFieldException e) {
+	    fail("Implementation of type \"" + obj.getClass().getSimpleName()
+		    + "\" misses a field named \"" + fieldName + "\"");
 	}
+	return value;
+    }
 
-	/**
-	 * Lets you access the values of private or protected fields in your tests.
-	 * You will have to cast the resulting object down to the real type.
-	 * 
-	 * @param obj
-	 * @param fieldName
-	 * @return
-	 */
-	public static Object getFieldValue(Object obj, String fieldName) {
-		Object value = null;
-		try {
-			Field f = obj.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			value = f.get(obj);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		} catch (SecurityException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		} catch (NoSuchFieldException e) {
-			fail("Implementation of type \"" + obj.getClass().getSimpleName()
-					+ "\" misses a field named \"" + fieldName + "\"");
-		}
-		return value;
+    public static Object callMethod(Object obj,
+				    String methodName,
+				    Class<?>[] parameterTypes,
+				    Object[] arguments) {
+	Object returnValue = null;
+	try {
+	    Method m = obj.getClass().getMethod(methodName,
+						parameterTypes);
+	    m.setAccessible(true);
+	    m.invoke(obj,
+		     arguments);
+	} catch (SecurityException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (NoSuchMethodException e) {
+	    StringBuffer signature = new StringBuffer();
+	    signature.append(methodName + "(");
+	    for (int i = 0; i < parameterTypes.length; i++) {
+		if (i > 0)
+		    signature.append(", ");
+		signature.append(parameterTypes[i].getName());
+	    }
+	    signature.append(")");
+	    fail("Implementation of type \"" + obj.getClass().getSimpleName()
+		    + "\" misses a method \"" + signature + "\"");
+	} catch (IllegalArgumentException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (IllegalAccessException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
+	} catch (InvocationTargetException e) {
+	    e.printStackTrace();
+	    throw new RuntimeException(e);
 	}
+	return returnValue;
+    }
+
+    public static String getResString(int id) {
+	return GeoQuestApp.getContext().getResources().getString(id);
+    }
 
 }
