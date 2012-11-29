9669a98bed7a78d4ea06719d45af34794725a579
Holger Muegge <muegge@acm.org>
2012-11-29 22:57:52 +0100

    url attribute and error handling added

--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/VideoPlay.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/VideoPlay.java
@@ -21,6 +21,8 @@ public class VideoPlay extends InteractiveMission {
     private static final String TAG = "VideoPlay";
 
     CharSequence filePath = null;
+    CharSequence uriString = null;
+    Uri uri = null;
 
     private VideoView videoView;
 
@@ -32,14 +34,25 @@ public class VideoPlay extends InteractiveMission {
 	this.setContentView(R.layout.videoplay);
 	videoView = (VideoView) this.findViewById(R.id.videoplay_videoview);
 	filePath = getMissionAttribute("file",
-				       XMLUtilities.NECESSARY_ATTRIBUTE);
+				       XMLUtilities.OPTIONAL_ATTRIBUTE);
 	if (filePath == null) {
-	    Log.e(TAG,
-		  "Attribute file missing in VideoPlay Mission " + mission.id);
-	    finish();
+	    // attribute FILE NOT given => try to use URL:
+	    try {
+		uriString = getMissionAttribute("url",
+						XMLUtilities.NECESSARY_ATTRIBUTE);
+	    } catch (IllegalArgumentException iae) {
+		Log.e(TAG,
+		      "Both attributes file and url missing in VideoPlay Mission "
+			      + mission.id + ". At least one must be given.",
+		      iae);
+		finish();
+	    }
+	    uri = Uri.parse(uriString.toString());
+	} else {
+	    // attribute FILE given:
+	    uri = Uri.fromFile(GeoQuestApp.getGameRessourceFile(filePath
+		    .toString()));
 	}
-	Uri uri = Uri.fromFile(GeoQuestApp.getGameRessourceFile(filePath
-		.toString()));
 	videoView.setVideoURI(uri);
 	MediaController mc = new MediaController(this);
 	mc.setPrevNextListeners(finishButtonClickListener,
@@ -55,7 +68,40 @@ public class VideoPlay extends InteractiveMission {
 		    }
 
 		});
+	videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+
+	    public boolean onError(MediaPlayer mp,
+				   int what,
+				   int extra) {
+		Log.e(TAG,
+		      "Problem occurred when trying to play the video: "
+			      + uri.toString());
+		showErrorAlertDialog();
+		return true;
+	    }
+	});
 	videoView.start();
+    }
+
+    private void showErrorAlertDialog() {
+	AlertDialog exitDialog = new AlertDialog.Builder(VideoPlay.this)
+		.setTitle(R.string.error_dialog_title)
+		.setMessage(R.string.videoplay_errordialog_message)
+		.setNegativeButton(R.string.button_text_proceed,
+				   new DialogInterface.OnClickListener() {
+				       public void
+					       onClick(DialogInterface dialog,
+						       int which) {
+					   finish(Globals.STATUS_SUCCESS);
+				       }
+				   }).show();
+	exitDialog
+		.getButton(AlertDialog.BUTTON_NEGATIVE)
+		.setCompoundDrawablesWithIntrinsicBounds(null,
+							 null,
+							 getResources()
+								 .getDrawable(R.drawable.icon_leave),
+							 null);
     }
 
     private void showExitAlertDialog() {
