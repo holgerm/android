9bfa845e536055eaf1d8bb74131ec19db82fc967
Holger Muegge <muegge@acm.org>
2012-12-04 13:49:04 +0100

    New Resource Manager for single game session

---
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/GameSessionManager.java
@@ -0,0 +1,15 @@
+package edu.bonn.mobilegaming.geoquest;
+
+public class GameSessionManager {
+    
+    private String sessionID = null;
+
+    public String getSessionID() {
+	return sessionID;
+    }
+
+    public void setSessionID(String sessionID) {
+	this.sessionID = sessionID;
+    }
+
+}
