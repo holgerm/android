6b55f9074975a8324ef520427e3c9fa3ded5d2c7
Holger Muegge <muegge@acm.org>
2013-01-08 10:34:13 +0100

    documented, cleaned and modifier set

--- GQ_Android/src/com/qeevee/gq/history/TextItem.java
+++ GQ_Android/src/com/qeevee/gq/history/TextItem.java
@@ -8,25 +8,39 @@ import edu.bonn.mobilegaming.geoquest.GeoQuestActivity;
 import edu.bonn.mobilegaming.geoquest.GeoQuestApp;
 import edu.bonn.mobilegaming.geoquest.R;
 
+/**
+ * @author muegge
+ *
+ */
 public class TextItem extends HistoryItem {
 
     private CharSequence text;
 
+    /**
+     * Main constructor which should directly or indirectly be called by every
+     * other constructor of this class.
+     * 
+     * This constructor should be the only one who calls the super constructor.
+     * 
+     * @param textType
+     *            if you do not know what to do, use {@link TextType#DEFAULT}
+     *            which is {@link TextType#PLAIN}.
+     */
     public TextItem(CharSequence text,
-		    GeoQuestActivity activity) {
-	super(activity);
+		    GeoQuestActivity activity,
+		    TextType textType) {
+	super(activity, textType);
 	this.text = text;
     }
 
     public View getView(View convertView) {
 	TextView tv;
 	if (convertView == null) {
-	LayoutInflater inflater = (LayoutInflater) GeoQuestApp.getContext()
-		.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	tv = (TextView) inflater.inflate(R.layout.history_text_item,
-						  null);
-	}
-	else {
+	    LayoutInflater inflater = (LayoutInflater) GeoQuestApp.getContext()
+		    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    tv = (TextView) inflater.inflate(R.layout.history_text_item,
+					     null);
+	} else {
 	    tv = (TextView) convertView;
 	}
 	tv.setText(text);
