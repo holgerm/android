e3d481ae1cd494fd539f865a44979afa26a84dd8
Holger Muegge <muegge@acm.org>
2013-02-08 14:37:03 +0100

    xmlElement not stored in ui anymore. It's model stuff!

--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/ui/NPCTalkUI.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/ui/NPCTalkUI.java
@@ -1,22 +1,13 @@
 package edu.bonn.mobilegaming.geoquest.ui;
 
-import org.dom4j.Element;
-
-import android.view.View;
 import edu.bonn.mobilegaming.geoquest.mission.NPCTalk;
 
-public abstract class NPCTalkUI {
+public abstract class NPCTalkUI extends MissionUI {
 
-    NPCTalk activity = null;
-    View view = null;
-
-    public NPCTalkUI(Element xmlMissionElement,
-		     NPCTalk activity) {
-	this.activity = activity;
+    public NPCTalkUI(NPCTalk activity) {
+	super(activity);
     }
 
-    public View getView() {
-	return view;
-    }
+    public abstract boolean setImage(String pathToImageFile);
 
 }
