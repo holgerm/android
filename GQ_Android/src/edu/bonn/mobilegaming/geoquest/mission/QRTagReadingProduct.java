b669884699602e54a0985c644617c009373f34f5
Holger Muegge <muegge@acm.org>
2013-02-15 15:40:07 +0100

    TAG updated to new name

--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/QRTagReadingProduct.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/QRTagReadingProduct.java
@@ -74,7 +74,7 @@ import edu.bonn.mobilegaming.geoquest.Variables;
  */
 
 public class QRTagReadingProduct extends InteractiveMission implements OnClickListener {
-    private static final String TAG = "QRTagReading";
+    private static final String TAG = "QRTagReadingProduct";
 
     /** button to start the QRTag Reader */
     private Button okButton;
