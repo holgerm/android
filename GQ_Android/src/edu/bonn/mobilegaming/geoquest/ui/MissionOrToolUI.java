64dd4e3ae75e794e2abc4326d29e23620c35977d
Holger Muegge <muegge@acm.org>
2013-02-22 16:19:30 +0100

    blocking should be moved completely into ui types

---
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/ui/MissionOrToolUI.java
@@ -0,0 +1,9 @@
+package edu.bonn.mobilegaming.geoquest.ui;
+
+import edu.bonn.mobilegaming.geoquest.BlockableAndReleasable;
+
+
+public interface MissionOrToolUI extends BlockableAndReleasable {
+
+
+}
--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/ui/MissionUI.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/ui/MissionUI.java
@@ -1,11 +1,24 @@
 package edu.bonn.mobilegaming.geoquest.ui;
 
+import edu.bonn.mobilegaming.geoquest.BlockableAndReleasable;
 import edu.bonn.mobilegaming.geoquest.mission.MissionActivity;
 
-public abstract class MissionUI extends GeoQuestUI {
-    
+public abstract class MissionUI extends GeoQuestUI implements MissionOrToolUI {
+
     public MissionUI(MissionActivity activity) {
 	super(activity);
+	ibm = new InteractionBlockingManager(this);
+    }
+
+    protected InteractionBlockingManager ibm;
+
+    public BlockableAndReleasable
+	    blockInteraction(InteractionBlocker newBlocker) {
+	return ibm.blockInteraction(newBlocker);
+    }
+
+    public void releaseInteraction(InteractionBlocker blocker) {
+	ibm.releaseInteraction(blocker);
     }
 
 }
