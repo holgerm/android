82394e3841bed85ede5d4a242bb34fd44b4760d1
Holger Muegge <muegge@acm.org>
2013-02-22 16:18:30 +0100

    encaspulated nextbuttontext and formatted

--- GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/DialogItem.java
+++ GQ_Android/src/edu/bonn/mobilegaming/geoquest/mission/DialogItem.java
@@ -3,8 +3,6 @@ package edu.bonn.mobilegaming.geoquest.mission;
 import org.dom4j.Attribute;
 import org.dom4j.Element;
 
-import android.text.Html;
-
 /**
  * Dialog Items are needed by the NPC talk mission. A dialog item is for example
  * a sentence the NPC will say.
@@ -14,103 +12,101 @@ import android.text.Html;
  */
 public class DialogItem {
 
-	/** name of the speaker */
-	private String speaker;
-	/** DialogItems text */
-	private String text;
-	/** Array of words */
-	private String[] textElements;
-	/** name of the speaker in bold font */
-	private CharSequence formattedSpeaker;
-	/** used for iterating through the textElements */
-	private int counter = 0;
-	public CharSequence nextDialogButtonText = null;
-	private String audioFilePath;
-	public boolean blocking;
+    /** name of the speaker */
+    private String speaker;
+    /** DialogItems text */
+    private String text;
+    /** Array of words */
+    private String[] textElements;
+    /** used for iterating through the textElements */
+    private int counter = 0;
+    private CharSequence nextDialogButtonText = null;
 
-	/**
-	 * Constructor gets the information from the passed xmlElement
-	 * 
-	 * @param xmlElement
-	 *            the dialogItem element from the XML file
-	 */
-	public DialogItem(Element xmlElement) {
-		speaker = xmlElement.attributeValue("speaker");
+    public CharSequence getNextDialogButtonText() {
+	return nextDialogButtonText;
+    }
 
-		// read nextdialogbuttontext:
-		Attribute a = (Attribute) xmlElement
-				.selectSingleNode("@nextdialogbuttontext");
-		if (a != null) {
-			nextDialogButtonText = a.getText();
-		}
+    private String audioFilePath;
+    public boolean blocking;
 
-		// read sound (might be an audiofile for listenig to the text):
-		a = (Attribute) xmlElement.selectSingleNode("@sound");
-		if (a != null) {
-			audioFilePath = a.getText();
-		}
+    private Element xml = null;
 
-		// read blocking attribute:
-		a = (Attribute) xmlElement.selectSingleNode("@blocking");
-		if (a != null && a.getText().equals("false"))
-			blocking = false;
-		else
-			blocking = true;
+    /**
+     * Constructor gets the information from the passed xmlElement
+     * 
+     * @param xmlElement
+     *            the dialogItem element from the XML file
+     */
+    public DialogItem(Element xmlElement) {
+	xml = xmlElement;
+	speaker = xml.attributeValue("speaker");
 
-		text = xmlElement.getText().replaceAll("\\s+", " ").trim();
-		if (text.startsWith(" ")) {
-			text = text.substring(1);
-		}
-
-		if (speaker != null)
-			// just in case a speaker is given: add it in bold face before the
-			// text:
-			formattedSpeaker = Html.fromHtml("<b>" + speaker + ": </b>");
-		else
-			formattedSpeaker = "";
-
-		textElements = text.split("\\s+"); // Split anhand der Whitespaces
+	// read nextdialogbuttontext:
+	Attribute a = (Attribute) xml.selectSingleNode("@nextdialogbuttontext");
+	if (a != null) {
+	    nextDialogButtonText = a.getText();
 	}
 
-	/**
-	 * @return name of the speaker in bold font (HTML tags)
-	 */
-	public CharSequence getFormattedSpeaker() {
-		return formattedSpeaker;
+	// read sound (might be an audiofile for listenig to the text):
+	a = (Attribute) xml.selectSingleNode("@sound");
+	if (a != null) {
+	    audioFilePath = a.getText();
 	}
 
-	public String getSpeaker() {
-		return speaker;
+	// read blocking attribute:
+	a = (Attribute) xml.selectSingleNode("@blocking");
+	if (a != null
+		&& a.getText().equals("false"))
+	    blocking = false;
+	else
+	    blocking = true;
+
+	text = xml.getText().replaceAll("\\s+",
+					" ").trim();
+	if (text.startsWith(" ")) {
+	    text = text.substring(1);
 	}
 
-	public String getText() {
-		return text;
-	}
+	textElements = text.split("\\s+"); // Split anhand der Whitespaces
+    }
 
-	public String getAudioFilePath() {
-		return audioFilePath;
-	}
+    /**
+     * @return the speaker's name or <code>null</code> if no speaker given.
+     */
+    public String getSpeaker() {
+	return speaker;
+    }
 
-	/**
-	 * @return returns the number of words
-	 */
-	public int getNumParts() {
-		return textElements.length;
-	}
+    public String getText() {
+	return text;
+    }
 
-	/**
-	 * get the next word
-	 * 
-	 * @return returns each word as a CharSquence or null of there is no word
-	 *         left
-	 */
-	public CharSequence getNextPart() {
-		if (counter >= textElements.length)
-			return null;
-		if (counter == textElements.length - 1)
-			return textElements[counter++] + "\n";
+    public String getAudioFilePath() {
+	return audioFilePath;
+    }
 
-		return textElements[counter++] + " "; // add white space for all words
-												// but the last
-	}
+    /**
+     * @return returns the number of words
+     */
+    public int getNumParts() {
+	return textElements.length;
+    }
+
+    /**
+     * get the next word
+     * 
+     * @return returns each word as a CharSquence or null of there is no word
+     *         left
+     */
+    public CharSequence getNextPart() {
+	if (counter >= textElements.length)
+	    return null;
+	if (counter == textElements.length - 1)
+	    return textElements[counter++]
+		    + "\n";
+
+	return textElements[counter++]
+		+ " "; // add white space for all words
+		       // but the last
+    }
 }
